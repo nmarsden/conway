ae3f5986e0d37b607b9cfcdb4d2e8cd892f74cb4
Neil Marsden <neil.marsden@oracle.com>
2021-04-19 23:05:23 +1000

    - rewrite <Info/> as a functional component

--- src/components/info/index.tsx
+++ src/components/info/index.tsx
@@ -1,4 +1,4 @@
-import {Component, h} from 'preact';
+import {h} from 'preact';
 import style from './style.css';
 import {getPatternDisplayName, Pattern} from "../../utils/simulator";
 
@@ -7,18 +7,13 @@ type InfoProps = {
   generationNum: number;
 };
 
-type InfoState = {};
-
-class Info extends Component<InfoProps, InfoState> {
-
-  render(): JSX.Element {
-    return (
-      <div class={style['info']}>
-        <div class={style['pattern']}>{getPatternDisplayName(this.props.pattern)}</div>
-        <div class={style['generationNum']}>generation #<span>{this.props.generationNum}</span></div>
-      </div>
-    );
-  }
+function Info({pattern, generationNum}: InfoProps): JSX.Element {
+  return (
+    <div class={style['info']}>
+      <div class={style['pattern']}>{getPatternDisplayName(pattern)}</div>
+      <div class={style['generationNum']}>generation #<span>{generationNum}</span></div>
+    </div>
+  );
 }
 
 export default Info;
