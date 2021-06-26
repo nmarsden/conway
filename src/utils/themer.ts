a1219c8b89927a72658406cfc387ddea7524a3a3
Neil Marsden <neil.marsden@oracle.com>
2021-06-26 21:46:49 +1000

    fixed bug with icon images not being found when switching theme
    - no longer hard coding icon image urls in JS as these can change during the build which optimizes images
    - instead have a separate CSS variable for all dark and light theme icon urls which are read via JS

--- src/utils/themer.ts
+++ src/utils/themer.ts
@@ -5,24 +5,16 @@ const THEMES: Themes = {
     backgroundColor: "black",
     modalBackgroundColor: "rgba(0, 0, 0, 0.9)",
     accentColor: "hsl(213, 99%, 50%);",
-    closeIcon: "url('../../assets/icons/close-icon-dark.svg')",
-    gearIcon: "url('../../assets/icons/gear-icon-dark.svg')",
-    patternIcon: "url('../../assets/icons/pattern-icon-dark.svg')",
-    speedIcon: "url('../../assets/icons/speed-icon-dark.svg')",
-    trailIcon: "url('../../assets/icons/trail-icon-dark.svg')"
 },
   light: {
     textColor: "black",
     backgroundColor: "white",
     modalBackgroundColor: "rgba(255, 255, 255, 0.9)",
     accentColor: "hsl(213, 99%, 50%);",
-    closeIcon: "url('../../assets/icons/close-icon-light.svg')",
-    gearIcon: "url('../../assets/icons/gear-icon-light.svg')",
-    patternIcon: "url('../../assets/icons/pattern-icon-light.svg')",
-    speedIcon: "url('../../assets/icons/speed-icon-light.svg')",
-    trailIcon: "url('../../assets/icons/trail-icon-light.svg')"
   }
 };
+
+const ICONS = ["closeIcon", "gearIcon", "patternIcon", "speedIcon", "trailIcon"];
 
 type Themes = {
   dark: Theme;
@@ -34,22 +26,24 @@ type Theme = {
   backgroundColor: string;
   modalBackgroundColor: string;
   accentColor: string;
-  closeIcon: string;
-  gearIcon: string;
-  patternIcon: string;
-  speedIcon: string;
-  trailIcon: string;
 }
 
 export class Themer {
   static updateTheme(isDark: boolean): void {
-    Themer.setCSSVariables(isDark ? THEMES.dark : THEMES.light);
+    Themer.setCSSVariables(isDark);
   }
 
-  private static setCSSVariables(theme: Theme): void {
+  private static setCSSVariables(isDark: boolean): void {
+    const theme = isDark ? THEMES.dark : THEMES.light
     let key: keyof Theme;
     for (key in theme) {
       document.documentElement.style.setProperty(`--${key}`, theme[key]);
     }
+    // Set icon CSS variables
+    const themeSuffix = isDark ? "Dark" : "Light";
+    ICONS.forEach(icon => {
+      const iconValue = getComputedStyle(document.documentElement).getPropertyValue(`--${icon}${themeSuffix}`);
+      document.documentElement.style.setProperty(`--${icon}`, iconValue);
+    });
   }
 }
